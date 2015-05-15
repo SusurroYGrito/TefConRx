e39f005122ff4eff006fda5acbbec7dd3f375a90
Juan Agui <juan@juanagui.com>
2015-05-15 12:34:15 +0200

    adds sample of creating and consuming and observable via extension methods

---
+++ SimpleWithExtensionMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace SimpleWithExtensionMethods
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var numbers = Observable.Create<int>(observer =>
+            {
+                observer.OnNext(1);
+                observer.OnNext(2);
+                observer.OnNext(3);
+                observer.OnCompleted();
+                return Disposable.Empty;
+            });
+
+            Console.WriteLine("Press any key to subscribe");
+            Console.ReadKey();
+            numbers.Subscribe(
+                value => Console.WriteLine("Received value {0}", value),
+                error => Console.WriteLine("Sequence faulted with {0}", error),
+                () => Console.WriteLine("Sequence terminated"));
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
