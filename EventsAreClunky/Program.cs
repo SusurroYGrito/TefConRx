f6cff7b2fa3188ffc042acf1d187282921c8802d
Juan Agui <juan@juanagui.com>
2015-05-15 13:14:27 +0200

    adds events are clunky demo

---
+++ EventsAreClunky/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace EventsAreClunky
+{
+    public class EventSource
+    {
+        //Declaración de un evento
+        public event EventHandler<char> AnEvent;
+
+        public void Start()
+        {
+            while (true)
+            {
+                var c = Console.ReadKey(true);
+                AnEvent(this, c.KeyChar);
+            }
+        }
+    }
+
+    public class EventConsumer
+    {
+        public EventConsumer(EventSource eventSource)
+        {
+            //Subscripción a un evento
+            eventSource.AnEvent += AnEventHandler;
+        }
+
+        //Manejador del evento
+        private void AnEventHandler(object sender, char e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = new EventSource();
+            var consumer = new EventConsumer(source);
+            source.Start();
+        }
+    }
+}
